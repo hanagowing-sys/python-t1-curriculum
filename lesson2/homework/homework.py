2ba6bf8901d23f706fbcdfc26b6285f3a6af29c0
Chaemiee <hana.gowing@gmail.com>
2026-02-21 18:55:59 -0800

    Refactor number input prompts and add division output
    Updated user prompts for number input and added print statement for quotient.

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -1,17 +1,20 @@
 # Homework Problem 1
-# Ask the user for two numbers.
+# Ask the user for two numbers.num1 = int(input("Enter the first number: "))
 # Print their quotient and remainder on separate lines.
-
-
+num1 = int(input("Give me a number!"))
+num2 = int(input("Ooookay, so give me another number!"))
+print(num1 / num2)
 
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
-
+user_animal = input("Whats your favorite animal?")
+user_animal2 = input("Oh thats nice! Whats your favorite color?")
 
 
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
+# we didnt learn about loops yet, hopefully we'll learn about it nexzt lesson! -Hana
 
 
 
